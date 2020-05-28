e3c786007c57ad0a2b0a54740ec8a5bb4cb02ab4
Norbert Jurga <jurganorbert@gmail.com>
2020-05-28 14:55:13 +0200

    fixed chat width

--- resources/views/live.blade.php
+++ resources/views/live.blade.php
@@ -9,7 +9,7 @@
                 <iframe src="https://vimeo.com/event/75305/embed" frameborder="0" allow="autoplay; fullscreen" id="video-iframe" allowfullscreen style="top:0;left:0;width:100%;height:100%;"></iframe>
             </div>
             <div class="col-md-4">
-                <iframe src="https://vimeo.com/event/75305/chat/" width="400" height="600" frameborder="0"></iframe>
+                <iframe src="https://vimeo.com/event/75305/chat/" id="chat-iframe" style="top:0;left:0;width:100%;height:600px;" frameborder="0"></iframe>
             </div>
         </div>
     </div>
