bd7eebe9e2342dc32596ed2dc8209956e8624012
Norbert Jurga <jurganorbert@gmail.com>
2020-05-28 14:33:30 +0200

    google analytics support

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,18 @@
         <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
 
         <link rel="shortcut icon" href="storage/{{setting('site.favicon')}}"/>
+        
+        @if (setting('site.google_analytics_tracking_id'))
+            <!-- Global site tag (gtag.js) - Google Analytics -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id={{setting('site.google_analytics_tracking_id')}}"></script>
+            <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
+            gtag('config', "{{setting('site.google_analytics_tracking_id')}}");
+            </script>
+        @endif
     </head>
     <body>
         @yield('content')
