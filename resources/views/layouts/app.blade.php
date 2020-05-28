b78a43665ba0c986ea60968af1f033b832a9e181
Norbert Jurga <jurganorbert@gmail.com>
2020-05-28 12:47:04 +0200

    added meta no index

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>{{setting('site.title')}}</title>
-        <meta name="description" content="{{setting('site.description')}}"/>
+        <meta name="robots" content="noindex">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
