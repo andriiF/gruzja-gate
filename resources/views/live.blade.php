1bc5a92007ed0ed249288e2c45a28db7f423f0ad
Norbert Jurga <jurganorbert@gmail.com>
2020-05-28 14:47:20 +0200

    pixed mobile iframe

--- resources/views/live.blade.php
+++ resources/views/live.blade.php
@@ -6,7 +6,7 @@
         <div class="row">
             <div class="col-md-8">
                 <img class="dancer" src="{{asset('img/dancer.svg')}}" alt="">
-                <iframe src="https://vimeo.com/event/75305/embed" frameborder="0" allow="autoplay; fullscreen" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe>
+                <iframe src="https://vimeo.com/event/75305/embed" frameborder="0" allow="autoplay; fullscreen" id="video-iframe" allowfullscreen style="top:0;left:0;width:100%;height:100%;"></iframe>
             </div>
             <div class="col-md-4">
                 <iframe src="https://vimeo.com/event/75305/chat/" width="400" height="600" frameborder="0"></iframe>
