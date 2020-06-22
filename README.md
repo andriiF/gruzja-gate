6018b61dd04da5ed7df9a9f3e18401c6c4b32748
andrii <andrii@andrii.pl>
2020-06-22 17:38:44 +0200

    install instruction

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 2. Add to env second lines <br/>
 VIDEO=Link to wideo widget<br/>
 CHAT=Link to chat widget
+MAIN_PAGE_URL=http://url.lo/   
+EVENT_ID=24  id wydrzenia
+
+3. If it needed install admin
+php artisan voyager:install
+php artisan voyager:admin your@email.com --create
--- app/Http/Controllers/CodeController.php
+++ app/Http/Controllers/CodeController.php
@@ -5,23 +5,22 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Code;
 
-class CodeController extends Controller
-{
+class CodeController extends Controller {
+
     /**
      * Display the specified resource.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($code)
-    {
+    public function show($code) {
         $code = Code::where('code', $code)->first();
-        $can_use = $code &&  $code->times_used < 4;
+        $can_use = $code && $code->times_used < 4;
 
-        if($can_use){
+        if ($can_use) {
             return view('live');
-        }else{
-            $error = !$code ? __('messages.wrong_code') : __('messages.limit') ;
+        } else {
+            $error = !$code ? __('messages.wrong_code') : __('messages.limit');
             return view('welcome')->with(['error' => $error]);
         }
     }
@@ -32,18 +31,27 @@ class CodeController extends Controller
      * @param  int  $id
      * @param  \Illuminate\Http\Request  $request
      */
-
-    public function verify(Request $request)
-    {
+    public function verify(Request $request) {
         $code = Code::where('code', $request->input('code'))->first();
+        if ($code && 3 - $code->times_used > 0) {
 
-        if($code && 3 - $code->times_used > 0){
             $code->times_used += 1;
-            $code->save(); 
+            $code->save();
+            $this->sendRequestToMainPage($code);
+
             return redirect('/live/' . $code->code);
-        }else{
-            $error = !$code ? __('messages.wrong_code') : __('messages.limit') ;
+        } else {
+            $error = !$code ? __('messages.wrong_code') : __('messages.limit');
             return view('welcome')->with(['error' => $error]);
         }
     }
+
+    private function sendRequestToMainPage($code) {
+        $ch = curl_init(env('MAIN_PAGE_URL') . 'verifyEventCode/' . env('EVENT_ID') . '/' . $code->code);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        $data = curl_exec($ch);
+        curl_close($ch);
+    }
+
 }
