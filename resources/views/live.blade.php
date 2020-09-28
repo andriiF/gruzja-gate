1b5f18165e10b74d04561b1758d53f83f429fe64
andrii <andrii@andrii.pl>
2020-09-28 16:07:11 +0200

    poprawka rwd

--- resources/views/live.blade.php
+++ resources/views/live.blade.php
@@ -3,12 +3,14 @@
 <section class="live-section">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-9">
-                <div style="padding:56.25% 0 0 0;position:relative; width: 1280px; height: 720px">
+            <div class="col-md-6 col-xl-8">
+                <div class="video-container">
                     <iframe src="https://vimeo.com/event/275150/embed" frameborder="0" allow="autoplay; fullscreen" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div>
             </div>
-            <div class="col-md-3">
-                <iframe src="https://vimeo.com/event/275150/chat/" width="400" height="600" frameborder="0"></iframe>
+            <div class="col-md-6 col-xl-4">
+                <div class="chat-container">
+                    <iframe src="https://vimeo.com/event/275150/chat/" style="width: 100%; height: 100%" frameborder="0"></iframe>
+                </div>
             </div>
         </div>  
     </div>
