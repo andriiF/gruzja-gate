aa9e82526930857fe755ea6db2783a4194ea7f8b
andriiF <andreiif123@gmail.com>
2021-03-16 13:00:40 +0100

    admin preveliogios on page counter

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -3,16 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class WebController extends Controller {
 
     public function index() {
         $is_code = false;
+        
 
         $date = strtotime('25-03-2021 15:00:00');
         $now = strtotime(now());
 
-        if ($now > $date) {
+        if ($now > $date || Auth::user()) {
             $is_code = true;
         }
 
