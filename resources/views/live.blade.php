b58ccc43a66e4271f071f8a55737504569806d4e
andriiF <andreiif123@gmail.com>
2021-03-16 15:21:38 +0100

    dodanie iframe

--- resources/views/live.blade.php
+++ resources/views/live.blade.php
@@ -13,10 +13,7 @@
         <div class="row">
             <div class="col-12">
                 <div class="video-container">
-                </div>
-                <div class="col-md-6 col-xl-4">
-                    <div class="chat-container">
-                    </div>
+                    <div style="padding:90vh 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/524294460" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div>
                 </div>
             </div>  
         </div>
